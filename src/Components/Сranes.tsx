a3484d506682016e38ea390ab27ebb80b8d0c23e
user00777777 <misterborman@gmail.com>
2023-03-29 23:15:26 +0300

    f

--- src/Components/Сranes.tsx
+++ src/Components/Сranes.tsx
@@ -3,24 +3,46 @@ import s from '../CssModules/Cranes.module.css'
 
 export default function Сranes() {
   return (
-    <div><div className={s.parent}>
-    <div className={s.div1}> 1</div>
-    <div className={s.div2}> </div>
-    <div className={s.div3}> 3</div>
-    <div className={s.div4}> </div>
-    <div className={s.div5}> </div>
-    <div className={s.div6}> </div>
-    <div className={s.div7}> </div>
-    <div className={s.div8}> </div>
-    <div className={s.div9}> </div>
-    <div className={s.div10}> </div>
-    <div className={s.div11}> </div>
-    <div className={s.div12}> </div>
-    <div className={s.div13}> </div>
-    <div className={s.div14}> </div>
-    <div className={s.div15}> </div>
-    <div className={s.div16}> </div>
-    <div className={s.div17}> </div>
-    </div></div>
+    <div className={s.parent}>
+<div className={s.div1}>№51906</div>
+<div className={s.div2}>№52044 </div>
+<div className={s.div3}>№51915 </div>
+<div className={s.div4}>4 </div>
+<div className={s.div5}> </div>
+<div className={s.div6}> </div>
+<div className={s.div7}> </div>
+<div className={s.div8}> </div>
+<div className={s.div9}> </div>
+<div className={s.div10}> </div>
+<div className={s.div11}> </div>
+<div className={s.div12}> </div>
+<div className={s.div13}> </div>
+<div className={s.div14}> </div>
+<div className={s.div15}> </div>
+<div className={s.div16}> </div>
+<div className={s.div17}> </div>
+<div className={s.div18}> </div>
+<div className={s.div19}> </div>
+<div className={s.div20}> </div>
+<div className={s.div21}> </div>
+<div className={s.div22}> </div>
+<div className={s.div23}> </div>
+<div className={s.div24}> </div>
+<div className={s.div25}> </div>
+<div className={s.div26}> </div>
+<div className={s.div27}> </div>
+<div className={s.div28}> </div>
+<div className={s.div29}> </div>
+<div className={s.div30}> </div>
+<div className={s.div31}> </div>
+<div className={s.div32}> </div>
+<div className={s.div33}> </div>
+<div className={s.div34}> </div>
+<div className={s.div35}> </div>
+<div className={s.div36}> </div>
+<div className={s.div37}> </div>
+<div className={s.div38}> </div>
+<div className={s.div39}> </div>
+</div>
   )
 }
